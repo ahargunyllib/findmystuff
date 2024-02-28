c782ac7e7c546f6ab8aff873592ab56ebded9486
ghufron bagaskara <ghufronbagaskara08@gmail.com>
2024-02-28 15:52:36 +0700

    adding text in registerScreen

--- app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/auth/RegisterScreen.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/auth/RegisterScreen.kt
@@ -2,6 +2,7 @@ package com.ahargunyllib.internraion.features.presentation.screen.auth
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
@@ -9,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ahargunyllib.internraion.ui.theme.InternraionTheme
 
@@ -21,6 +23,7 @@ fun RegisterScreen(navController: NavController) {
                 verticalArrangement = Arrangement.Center
             ) {
                 Text(text = "Register")
+                Text(text = "this is the edit")
                 Button(onClick = { navController.popBackStack() }) {
                     Text("Back")
                 }

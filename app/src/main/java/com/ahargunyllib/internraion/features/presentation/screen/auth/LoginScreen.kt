acd99010a672e600566165757c8325cc927e18e9
ghufron bagaskara <ghufronbagaskara08@gmail.com>
2024-02-29 09:38:34 +0700

    adding skeleton ui for login and register

--- app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/auth/LoginScreen.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/auth/LoginScreen.kt
@@ -1,15 +1,36 @@
 package com.ahargunyllib.internraion.features.presentation.screen.auth
 
+import android.graphics.Paint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.ahargunyllib.internraion.ui.theme.InternraionTheme
 
 @Composable
@@ -17,14 +38,77 @@ fun LoginScreen(navController: NavController) {
     InternraionTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                modifier = Modifier.fillMaxSize()
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement = Arrangement.Center
             ) {
-                Text(text = "Login")
                 Button(onClick = { navController.popBackStack() }) {
                     Text("Back")
                 }
+                Text(text = "Login")
+
+                var username by rememberSaveable {
+                    mutableStateOf("")
+                }
+
+                Column {
+                    TextField(
+                        value = username,
+                        onValueChange = {
+                            username = it
+                        },
+                        label = {
+                            Text(text = "Username")
+                        }
+                    )
+                }
+                Spacer(modifier = Modifier.size(12.dp))
+
+                var password by rememberSaveable {
+                    mutableStateOf("")
+                }
+                var passwordVisible by rememberSaveable {
+                    mutableStateOf(false)
+                }
+                Column {
+                    TextField(
+                        value = password,
+                        onValueChange = {
+                            password = it
+                        },
+                        label = {
+                            Text(text = "Password")
+                        },
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password
+                        )
+                    )
+                }
+
+                Text(text = "Forget Password?")
+
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Login")
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Doesn't have an account?")
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Sign up")
+                    }
+                }
+
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    InternraionTheme {
+        LoginScreen(navController = rememberNavController())
     }
 }
--- app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/auth/RegisterScreen.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/auth/RegisterScreen.kt
@@ -2,16 +2,32 @@ package com.ahargunyllib.internraion.features.presentation.screen.auth
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.ahargunyllib.internraion.ui.theme.InternraionTheme
 
 @Composable
@@ -19,15 +35,118 @@ fun RegisterScreen(navController: NavController) {
     InternraionTheme {
         Surface(modifier = Modifier.fillMaxSize()) {
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                modifier = Modifier.fillMaxSize(),
+//                horizontalAlignment = Alignment.CenterHorizontally,
+//                verticalArrangement = Arrangement.Center
             ) {
-                Text(text = "Register")
-                Text(text = "this is the edit")
                 Button(onClick = { navController.popBackStack() }) {
                     Text("Back")
                 }
+                Text(text = "Register")
+
+                var emailAdress by rememberSaveable {
+                    mutableStateOf("")
+                }
+                Column {
+                    TextField(
+                        value = emailAdress,
+                        onValueChange = {
+                            emailAdress = it
+                        },
+                        label = {
+                            Text(text = "Email Address")
+                        }
+                    )
+                }
+                Spacer(modifier = Modifier.size(12.dp))
+
+                var username by rememberSaveable {
+                    mutableStateOf("")
+                }
+                Column {
+                    TextField(
+                        value = username,
+                        onValueChange = {
+                            username = it
+                        },
+                        label = {
+                            Text(text = "Username")
+                        }
+                    )
+                }
+
+                Spacer(modifier = Modifier.size(12.dp))
+
+                var password by rememberSaveable {
+                    mutableStateOf("")
+                }
+                var passwordVisible by rememberSaveable {
+                    mutableStateOf(false)
+                }
+                Column {
+                    TextField(
+                        value = password,
+                        onValueChange = {
+                            password = it
+                        },
+                        label = {
+                            Text(text = "Password")
+                        },
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password
+                        )
+                    )
+                }
+
+                Spacer(modifier = Modifier.size(12.dp))
+
+                var confirmPassword by rememberSaveable {
+                    mutableStateOf("")
+                }
+                var confirmPassVisible by rememberSaveable {
+                    mutableStateOf(false)
+                }
+                Column {
+                    TextField(
+                        value = confirmPassword,
+                        onValueChange = {
+                            confirmPassword = it
+                        },
+                        label = {
+                            Text(text = "Confirm Password")
+                        },
+                        visualTransformation = if (confirmPassVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Password
+                        )
+                    )
+                }
+
+                Text(text = "Sign up with")
+
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Sign up")
+                }
+
+                val checked = remember {
+                    mutableStateOf(false)
+                }
+                Row {
+                    Checkbox(checked = checked.value, onCheckedChange = { checked.value = it })
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Terms and Conditions")
+                    }
+                }
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun RegisterScreenPreview() {
+    InternraionTheme {
+        RegisterScreen(navController = rememberNavController())
     }
 }

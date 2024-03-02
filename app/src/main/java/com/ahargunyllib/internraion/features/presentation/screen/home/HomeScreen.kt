e719f135f0ab1b414a831a06b438f09e904d60fb
ghufron bagaskara <ghufronbagaskara08@gmail.com>
2024-03-02 11:59:52 +0700

    adding logic register -> login, login -> home, and pop up in register

--- app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.ahargunyllib.internraion.features.presentation.screen.home
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
@@ -11,6 +12,9 @@ fun HomeScreen(navController: NavController) {
     InternraionTheme {
         Row {
             Text(text = "Home Screen")
+            Button(onClick = { navController.navigate("maps") }) {
+                Text(text = "to maps")
+            }
         }
     }
 }

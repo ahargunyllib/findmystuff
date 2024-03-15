8b52b8c67122488d5a886213cd2727399e74dda5
ahargunyllib <billy.bpm03@gmail.com>
2024-03-15 22:39:55 +0700

    fix sesion bug

--- app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/home/HomeScreen.kt
@@ -2,35 +2,46 @@ package com.ahargunyllib.internraion.features.presentation.screen.home
 
 import android.annotation.SuppressLint
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.ahargunyllib.internraion.features.data.network.SupabaseClient
 import com.ahargunyllib.internraion.features.data.repository.user.UserRepository
+import com.ahargunyllib.internraion.features.data.utils.Response
 import com.ahargunyllib.internraion.features.presentation.navigation.BottomNavigationBar
 import com.ahargunyllib.internraion.utils.Routes
+import kotlinx.coroutines.Delay
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(navController: NavController) {
     val viewModel = HomeViewModel(userRepository = UserRepository(supabaseClient = SupabaseClient))
-    val context = LocalContext.current
+    val state = viewModel.state.collectAsState()
 
     LaunchedEffect(Unit) {
-        viewModel.isUserLoggedIn(
-            context
-        )
+        viewModel.isUserLoggedIn()
+    }
 
-        if (!viewModel.isUserLoggedIn) {
-            Log.i("home screen", "HomeScreen: user not login")
+    when (state.value) {
+        is Response.Loading -> {}
+
+        is Response.Success -> {}
+
+        is Response.Error -> {
             navController.navigate(Routes.WELCOME)
         }
     }
+
+
     Scaffold(
         bottomBar = { BottomNavigationBar(navController = navController) }
     ) {
--- app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/home/HomeViewModel.kt
@@ -2,46 +2,30 @@ package com.ahargunyllib.internraion.features.presentation.screen.home
 
 import android.content.Context
 import android.util.Log
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ahargunyllib.internraion.features.data.repository.user.UserRepository
+import com.ahargunyllib.internraion.features.data.utils.Response
 import com.ahargunyllib.internraion.utils.SharedPreferenceHelper
 import io.github.jan.supabase.gotrue.user.UserInfo
 import io.github.jan.supabase.gotrue.user.UserSession
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val userRepository: UserRepository): ViewModel() {
-    var isUserLoggedIn by mutableStateOf(false)
+    private val _state = MutableStateFlow<Response>(Response.Loading)
+    val state = _state.asStateFlow()
 
-    fun isUserLoggedIn(context: Context){
+    fun isUserLoggedIn(){
         viewModelScope.launch {
-            val sharedPref = SharedPreferenceHelper(context)
-            var accessToken = sharedPref.getStringData("accessToken")
-            var refreshToken = sharedPref.getStringData("refreshToken")
-
-            if (accessToken.isNullOrEmpty() ||  refreshToken.isNullOrEmpty()) isUserLoggedIn = false
-            else {
-                isUserLoggedIn = try {
-                    val user = userRepository.retrieveUser(accessToken)
-                    val userSession = UserSession(accessToken = accessToken, refreshToken = refreshToken, expiresIn = 60, tokenType = "Bearer", user = user)
-                    userRepository.importSession(userSession)
-
-                    accessToken = userRepository.getAccessToken()
-                    refreshToken = userRepository.getRefreshToken()
-
-                    sharedPref.saveStringData("accessToken", accessToken)
-                    sharedPref.saveStringData("refreshToken", refreshToken)
-                    true
-                } catch (e: Exception){
-                    Log.i("home view model", "isUserLoggedIn: $e")
-                    false
-                }
+            userRepository.isUserLoggedIn().collect {
+                _state.value = it
             }
         }
     }
-
-
 }

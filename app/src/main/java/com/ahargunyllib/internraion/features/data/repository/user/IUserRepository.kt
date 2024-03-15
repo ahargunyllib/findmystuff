cba1bb38eb8530d132d1ee653e5d0a79419557f2
ahargunyllib <billy.bpm03@gmail.com>
2024-03-15 22:39:45 +0700

    fix login bug

--- app/src/main/java/com/ahargunyllib/internraion/features/data/repository/user/IUserRepository.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/data/repository/user/IUserRepository.kt
@@ -9,15 +9,7 @@ interface IUserRepository {
     suspend fun signUpUser(email: String, password: String, fullName: String): Flow<Response>
     suspend fun signInUser(email: String, password: String): Flow<Response>
     suspend fun signOutUser()
+    suspend fun isUserLoggedIn(): Flow<Response>
 
-    suspend fun getAccessToken(): String?
-    suspend fun getRefreshToken(): String?
-
-    suspend fun getUserSession(): UserSession?
-
-    suspend fun retrieveUser(accessToken: String): UserInfo
-
-    suspend fun refreshSession(refreshToken: String): UserSession
-
-    suspend fun importSession(userSession: UserSession)
+    suspend fun getAccessToken(): String
 }
--- app/src/main/java/com/ahargunyllib/internraion/features/data/repository/user/UserRepository.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/data/repository/user/UserRepository.kt
@@ -3,6 +3,7 @@ package com.ahargunyllib.internraion.features.data.repository.user
 import com.ahargunyllib.internraion.features.data.network.SupabaseClient
 import com.ahargunyllib.internraion.features.data.utils.Response
 import com.ahargunyllib.internraion.features.domain.model.User
+import com.ahargunyllib.internraion.utils.SharedPreferenceHelper
 import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.gotrue.providers.builtin.Email
 import io.github.jan.supabase.gotrue.user.UserInfo
@@ -51,27 +52,21 @@ class UserRepository(
         supabaseClient.client.auth.signOut()
     }
 
-    override suspend fun getAccessToken(): String? {
-        return supabaseClient.client.auth.currentSessionOrNull()?.accessToken
+    override suspend fun isUserLoggedIn(): Flow<Response> = channelFlow {
+        try {
+            send(Response.Loading)
+            val userSession = supabaseClient.client.auth.currentSessionOrNull()
+            if (userSession != null) {
+                send(Response.Success("User is logged in"))
+            } else {
+                send(Response.Error("User is not logged in"))
+            }
+        } catch (e: Exception) {
+            send(Response.Error(e.message ?: ""))
+        }
     }
 
-    override suspend fun getRefreshToken(): String? {
-        return supabaseClient.client.auth.currentSessionOrNull()?.refreshToken
-    }
-
-    override suspend fun getUserSession(): UserSession? {
-        return supabaseClient.client.auth.currentSessionOrNull()
-    }
-
-    override suspend fun retrieveUser(accessToken: String): UserInfo {
-        return supabaseClient.client.auth.retrieveUser(accessToken)
-    }
-
-    override suspend fun refreshSession(refreshToken: String): UserSession {
-        return supabaseClient.client.auth.refreshSession(refreshToken = refreshToken)
-    }
-
-    override suspend fun importSession(userSession: UserSession) {
-        supabaseClient.client.auth.importSession(userSession)
+    override suspend fun getAccessToken(): String {
+        return supabaseClient.client.auth.currentSessionOrNull()?.accessToken ?: ""
     }
 }

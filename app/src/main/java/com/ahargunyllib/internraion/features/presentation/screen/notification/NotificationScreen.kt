1aab14d9f53d1a554d1ee6f74ee3a75a0206b807
ahargunyllib <billy.bpm03@gmail.com>
2024-03-21 15:31:11 +0700

    fixing NotificationScreen

--- app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/notification/NotificationScreen.kt
+++ app/src/main/java/com/ahargunyllib/internraion/features/presentation/screen/notification/NotificationScreen.kt
@@ -1,9 +1,11 @@
 package com.ahargunyllib.internraion.features.presentation.screen.notification
 
 import android.annotation.SuppressLint
+import android.app.Notification
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +19,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForwardIos
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -36,6 +40,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.ahargunyllib.internraion.R
+import com.ahargunyllib.internraion.ui.component.SingleIconTopBar
 import com.ahargunyllib.internraion.ui.theme.Green
 import com.ahargunyllib.internraion.ui.theme.Type
 
@@ -44,94 +49,79 @@ import com.ahargunyllib.internraion.ui.theme.Type
 fun NotificationScreen(navController: NavController) {
     Scaffold(
         topBar = {
-            Row(
+            SingleIconTopBar(
+                title = "NOTIFIKASI",
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_back_button),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(30.dp)
+                            .clickable { navController.popBackStack() },
+                        tint = Green
+                    )
+                }
+            )
+        },
+        content = {
+            LazyColumn(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(57.dp)
-                    .shadow(elevation = 4.dp)
-                    .background(Color.White),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
+                    .padding(top = 64.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_back_button),
-                    contentDescription = "",
-                    modifier = Modifier
-                        .size(30.dp)
-                        .padding(start = 5.dp, top = 5.dp)
-                        .clickable { navController.popBackStack() },
-                    tint = Green
-                )
-                Text(
-                    text = "NOTIFIKASI",
-                    style = Type.textMedium(),
-                    fontSize = 17.sp,
-                    fontWeight = FontWeight(600),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                )
-
+                items(15) {
+                    NotificationItem()
+                    HorizontalDivider()
+                }
             }
         }
-    ) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(start = 10.dp, top = 64.dp, bottom = 3.dp, end = 10.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            items(15) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-                    Row(
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(
-                            start = 9.dp,
-                            end = 9.dp,
-                            top = 5.dp,
-                            bottom = 5.dp
-                        )
-                    ) {
-                        AsyncImage(
-                            model = R.drawable.dummy_avatar,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(width = 55.dp, height = 55.dp)
-                                .clip(shape = RoundedCornerShape(100))
-                                .background(Color.Gray, shape = RoundedCornerShape(64.dp)),
-                            contentScale = ContentScale.Crop
-                        )
-                        Spacer(modifier = Modifier.size(15.dp))
-                        Column {
-                            Text(
-                                text = " 'XX' sepertinya menemukan barang kamu !!!",
-                                style = Type.notifFirst()
-                            )
-                            Row(
-                                modifier = Modifier.fillMaxWidth(),
-                                horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(text = "12 June 2023 19:34", style = Type.notifSecond())
-                                IconButton(onClick = { /*TODO*/ }) {
-                                    Icon(
-                                        imageVector = Icons.Default.ArrowForwardIos,
-                                        contentDescription = null,
-                                        modifier = Modifier
-                                            .padding(1.dp)
-                                            .width(12.dp)
-                                            .height(12.dp)
-                                    )
-                                }
+    )
+}
 
-                            }
-                        }
-                    }
+@Composable
+fun NotificationItem() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(
+                horizontal = 16.dp,
+                vertical = 8.dp
+            )
+        ) {
+            AsyncImage(
+                model = R.drawable.dummy_avatar,
+                contentDescription = "User Avatar",
+                modifier = Modifier
+                    .size(width = 64.dp, height = 64.dp)
+                    .clip(shape = RoundedCornerShape(100))
+                    .background(
+                        Color.Gray, shape = RoundedCornerShape(64.dp)
+                    ),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column(
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "'XX' sepertinya menemukan barang kamu !!!",
+                    style = Type.notifFirst()
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Text(text = "12 June 2023 19:34", style = Type.notifSecond())
                 }
             }
         }
